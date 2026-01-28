756cb863b305cbcf60b67ab9f9a803f6f85d88d3
mehdi bounouif <mehdibonouif@gmail.com>
2026-01-28 17:32:45 +0100

    path config

--- config/base_config.py
+++ config/base_config.py
@@ -11,4 +11,9 @@ class BaseConfig:
     ALPHA_VANTAGE_API_KEY = os.getenv('ALPHA_VANTAGE_API_KEY')
 
     DATABASE_URL = os.getenv('DATABASE_URL', 'sqlite:///data/trading_data.db')
-    
+   
+   PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+   DATA_DIR = os.path.join(PROJECT_ROOT, 'data')
+   RAW_DATA_DIR = os.path.join(DATA_DIR, 'raw')
+   PROCESSED_DATA_DIR = os.path.join(DATA_DIR, 'processed')
+
