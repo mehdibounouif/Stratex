4eded370404c846ac7062eb973bfc123c73539c0
mehdi bounouif <mehdibonouif@gmail.com>
2026-01-29 15:44:25 +0100

    add commit for data config 'abdilah'

--- config/data_config.py
+++ config/data_config.py
@@ -1,8 +1,14 @@
 class DataConfig:
+    # Data source
     DEFAULT_DATA_SOURCE = 'yfinance'
+    # Update time
     DATA_UPDATE_TIME = '16:30'
+    # Historical data
     DEFUALT_HISTORY_DAYS = 365
+    # Alpha vantage rate limits per minite
     ALPHA_VANTAGE_CALLS_PER_MINUTE = 5
+    # Alpha vantage rate limits per day
     ALPHA_VANTAGE_CALLS_PER_DAY = 25
+    # Data quality
     MAX_MISSING_DATA = 0.05
 
