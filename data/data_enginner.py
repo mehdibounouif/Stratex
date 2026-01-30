50ebb3ac1ef440ac0fb260fa990947f55faba537
mehdi bounouif <mehdibonouif@gmail.com>
2026-01-30 11:19:14 +0100

    get historical data

--- data/data_enginner.py
+++ data/data_enginner.py
@@ -18,4 +18,40 @@ class DataEngineer:
         - Add caching
         - Handle API rate limits
         """
+        print(f"Fetching {ticker} data (last {days} days)...")
+        try:
+            end_date = datetime.now()
+            start_data = end_date = timedelta(days=days)
+            data = yf.download(
+                    ticker,
+                    start= start_date.strftime('%Y-%m-%d'),
+                    end= end_date.strftime('%Y-%m-%d'),
+                    progress=False
+            )
+            if data.empy:
+                print(f"NO data found for {ticker}")
+                return (None)
+            print(f"Retrieved {len(data)} records for {ticker}")
+            return (data)
+
+        except Exeption as e:
+            print(f"Error fetching {ticker}: {e}")
+            return (None)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
