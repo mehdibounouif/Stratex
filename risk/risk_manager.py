bf32be64be30e3c50a197db4c35c889a8e2501b1
mehdi bounouif <mehdibonouif@gmail.com>
2026-01-31 23:01:48 +0100

    approve trade function

--- risk/risk_manager.py
+++ risk/risk_manager.py
@@ -19,3 +19,56 @@ class RiskManager:
         if cash_pct < self.config.MIN_CASH_RESERVE:
             return False, f"insuffcient cash reserve would have {cash_pct:.1%}"
         return True, "cash reserve Ok"
+    def approve_trade(self, trade):
+        print(f"\n Reviewing trade: {trade['ticker']}")
+        checks = {}
+        if trade['action'] == 'BUY':
+            trade_value = trade.get('quantity', 0) * trade.get('currect_price', 0)
+            position_size = trade_value / self.current_portfolio_value
+
+            """check position size"""
+            passed, msg = self.check_position_size(trade_value)
+            checks['position_size'] = passed
+            print(f"{msg}")
+
+            """check cash reserve"""
+            passed, msg = self.check_cash_reserve(trade_value)
+            checks['cash_reserve'] = passed
+            print(f"{msg}")
+
+            """check max position"""
+            if self.num_positions >= self.config.MAX_TOTAL_POSITION:
+                checks['max_position'] = False
+                print(f"{self.num_positions} is too much, Max positions is: {self.config.MAX_TOTAL_POSITIONS}")
+            else:
+                checks['max_position'] = True
+                print(f"{self.num_positions} is good, Max positions is: {self.config.MAX_TOTAL_POSITIONS}")
+
+        else: # sell of hold b3aybach logic
+            checks = {'position_size': True, 'cash_reserve': True, 'max_positions': True}
+            print("SELL/HOLD order - checks passed")
+        aproved = all(checks.values())
+        if aproved:
+            print("trade APPROVED")
+        else:
+            print("Trade REJECTED")
+
+        return {
+            'trade': trade,
+            'checks': checks,
+            'approved': aproved
+        }
+    
+risk_manager = RiskManager()
+
+if __name__ == "__main__":
+    print("Testing risk manager...")
+    test_trade = {
+        'ticker': 'AAPL',
+        'action': 'BUY',
+        'quantity': 7,
+        'current_price': 25.00
+    }
+
+    result = risk_manager.approve_trade(test_trade)
+    print(f"\nResult: {result}")
