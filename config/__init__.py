8bfd02a1d4fb1b228b72db9db9f8c6bcbe9e6583
mehdi bounouif <mehdibonouif@gmail.com>
2026-01-31 14:59:50 +0100

    add risk config in config package

--- config/__init__.py
+++ config/__init__.py
@@ -1,5 +1,6 @@
 from .base_config import BaseConfig
 from .data_config import DataConfig
 from .trading_config import TradingConfig
+from .risk_config import RiskConfig
 
-__all__ = ['BaseConfig', 'DataConfig', 'TradingConfig']
+__all__ = ['BaseConfig', 'DataConfig', 'TradingConfig', 'RiskConfig']
