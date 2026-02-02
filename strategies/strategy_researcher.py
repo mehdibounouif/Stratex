deb0fa8bf92bf11646b5703bf64c9733eea81170
mehdi bounouif <mehdibonouif@gmail.com>
2026-02-02 10:48:15 +0100

    create RSI Strategy class

---
+++ strategies/strategy_researcher.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+from config import TradingConfig
+
+class StrategyResearcher:
+    def __init__(self):
+        self.config = TradingConfig()
+        print("Using test strategy")
+    
