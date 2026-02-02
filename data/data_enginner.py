534173032fcc097a067fe376deacf6fa06cab3a3
mehdi bounouif <mehdibonouif@gmail.com>
2026-02-02 12:00:52 +0100

    fix some stuff

--- data/data_enginner.py
+++ data/data_enginner.py
@@ -49,6 +49,8 @@ data_access = DataEngineer()
 if __name__ == "__main__":
     print("Testing Data Enginner...")
     data = data_access.get_price_history('AAPL', days=30)
+#    print(f"data column : {data.columns}")
+#    print(f"data type : {type(data.columns)}")
     print(f"\nSample data:\n {data}")
 
 
--- strategies/strategy_researcher.py
+++ strategies/strategy_researcher.py
@@ -11,7 +11,7 @@ class StrategyResearcher:
         """Calculate RSI indicator"""
         delta = prices.diff()
         gain = (delta.where(delta > 0, 0)).rolling(window=period).mean()
-        loss = (delta.where(delta < 0, 0)).rolling(window=period).mean()
+        loss = (-delta.where(delta < 0, 0)).rolling(window=period).mean()
 
         rs = gain / loss
         rsi = 100 - (100 / (1 + rs))
@@ -20,9 +20,13 @@ class StrategyResearcher:
     def rsi_strategy(self, ticker, price_data):
         if price_data is None or price_data.empty:
             return (None)
-        rsi = self.calculate_rsi(price_data['Close'])
-        current_rsi = rsi.iloc[-1]
-        current_price = price_data['Close'].iloc[-1]
+        
+        close = price_data['Close']
+        if isinstance(close, pd.DataFrame):
+            close = close.iloc[:, 0]
+        rsi = self.calculate_rsi(close)
+        current_rsi = float(rsi.iloc[-1])
+        current_price = float(close.iloc[-1])
 
         if current_rsi < 30:
             signal = 'BUY'
@@ -48,7 +52,7 @@ class StrategyResearcher:
         }
     
     def analyze(self, ticker, price_data):
-        return self.simple_rsi_strategy(ticker, price_data)
+        return self.rsi_strategy(ticker, price_data)
 
 strategy_engine = StrategyResearcher()
 
@@ -56,5 +60,7 @@ if __name__ == "__main__":
     from data.data_enginner import data_access
     print("Testing Stratgy...")
     data = data_access.get_price_history('AAPL', days=90)
+    if data is None:
+        exit()
     signal = strategy_engine.analyze('AAPL', data)
     print(f"\n Signal: {signal}")
