7ac29ebcaaa9dd617dc8a8d4240dddbcbc8068e6
mehdi bounouif <mehdibonouif@gmail.com>
2026-02-02 12:32:51 +0100

    trading system class

---
+++ system/system_architect.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+from config import BaseConfig, TradingConfig, RiskConfig
+from data.data_enginner import data_access
+from strategies.strategy_researcher import strategy_engine
+from risk.risk_manager import risk_manager
+
+class TradingSystem:
+    pass
